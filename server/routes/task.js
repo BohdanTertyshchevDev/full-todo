31ed74b78c972803f1eee70f919b4bec87eff11a
Bohdan Tertyshchev <bobterya@gmail.com>
2023-05-19 17:31:23 +0300

    Token auth backend

--- server/routes/task.js
+++ server/routes/task.js
@@ -2,8 +2,9 @@ const Router = require('express');
 const taskRouter = Router();
 
 const TaskController = require('../controllers/task.controller');
+const {checkToken} = require('../middlewares/checkToken');
 
-taskRouter.post('/', TaskController.createUserTask);
-taskRouter.get('/:userId', TaskController.getAllUserTasks);
+taskRouter.post('/', checkToken, TaskController.createUserTask);
+taskRouter.get('/', checkToken, TaskController.getAllUserTasks);
 
 module.exports = taskRouter;
